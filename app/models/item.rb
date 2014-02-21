f20961fe888cb4da46afc36e3c9952c700a59d67
Bobby Uhlenbrock <bobby@uhlenbrock.us>
2014-02-21 16:09:00 +0000

    Created the item scaffold

---
+++ app/models/item.rb
@@ -0,0 +1,10 @@
+class Item < ActiveRecord::Base
+  
+  # -------------------------------------- Validations
+  
+  validates_presence_of :title
+  validates_presence_of :description
+  validates :price, numericality: { only_integer: true, greater_than: 0 }
+  validates_uniqueness_of :title, case_sensitive: false
+  
+end
