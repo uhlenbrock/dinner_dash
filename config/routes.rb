85ebb55213d90ae6657f8f50737399c830a4691a
Bobby Uhlenbrock <bobby@uhlenbrock.us>
2014-03-07 15:51:08 +0000

    Added menu controller and category/item partials

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ DinnerDash::Application.routes.draw do
   
   resources :categories
   resources :items
+  resources :menu, only: [:index]
   
   get  'about', to: 'marketing#about'
   get  'faqs',  to: 'marketing#faqs'
